701cd78a62018f37c08adb3efc008d222e68391d
fracolai <fc2e14@soton.ac.uk>
2014-11-19 18:12:23 +0000

    up

---
+++ Results_page.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(50) NULL, 
+    [Location] VARCHAR(50) NULL, 
+    [Salary] VARCHAR(50) NULL, 
+    [Job Type] VARCHAR(50) NULL
+)
