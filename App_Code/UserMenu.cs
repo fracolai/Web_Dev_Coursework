08c007aec58bc78d2fa1b9a3fcc84c5cdcb4ce7b
Soumya Sundarrajan <soumya.rajan86@gmail.com>
2014-11-20 20:32:30 +0000

    Creating data access layer
    Creating data access layer

---
+++ App_Code/UserMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Summary description for UserMenu
+/// </summary>
+namespace Web_Dev_Coursework
+{
+    public class UserMenu
+    {
+        [ScaffoldColumn(false)]
+        public int MenuItemID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string MenuItemName { get; set; }
+
+        [Display(Name = "Menu Item Description")]
+        public string Description { get; set; }
+
+        public virtual ICollection<UserInfo> UserInfo { get; set; }
+    }
+}
